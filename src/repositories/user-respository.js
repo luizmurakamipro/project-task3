986c186bb5f7cf22dfa65a2d6f65bcb1c811299e
Daniel Moya da Silva <danielm.silva@univem.edu.br>
2021-05-07 21:07:11 -0300

    Update user-respository.js

--- src/repositories/user-respository.js
+++ src/repositories/user-respository.js
@@ -9,7 +9,7 @@ exports.get = async (id) => {
 // Put
 exports.put = async (id, data) => {
     const res = await User.findOneAndUpdate(id, {
-        $set:{
+        $set: {
             name: data.name,
             email: data.email,
             password: data.password
